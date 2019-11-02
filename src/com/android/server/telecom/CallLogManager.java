df86cedef6d52435369b8a2f33d664fa11ac50fc
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-02 18:52:04 +0100

    Telecom: Add sensitive phone numbers hooks
    Change-Id: I809ceb65d7e188eb09dd4d85b5fdd716c63c60ef
    
    Telecom: Convert to using singleton class
    
    * SenstivePhoneNumbers is now a singleton, so change the
      calls accordingly
    
    Change-Id: I6377f42e4b9c11c0e74345cc65d50476d3c911ba
    
    Telecom: Adapt to the new location of SensitivePhoneNumbers
    
    * For an explanation, see the other patch(es) of the topic
    
    Change-Id: I45f2372dcf3aa20a2606bfb91e3763e2e372d0fc
    
    Change-Id: I49406eb14a7df6e714b6a0948444173285904983

--- src/com/android/server/telecom/CallLogManager.java
+++ src/com/android/server/telecom/CallLogManager.java
@@ -53,6 +53,8 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.server.telecom.callfiltering.CallFilteringResult;
 import com.android.server.telecom.flags.FeatureFlags;
 
+import org.lineageos.lib.phone.SensitivePhoneNumbers;
+
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Objects;
@@ -118,6 +120,7 @@ public final class CallLogManager extends CallsManagerListenerBase {
 
     private Object mLock;
     private String mCurrentCountryIso;
+    private SensitivePhoneNumbers mSensitivePhoneNumbers;
 
     private final FeatureFlags mFeatureFlags;
 
@@ -132,6 +135,7 @@ public final class CallLogManager extends CallsManagerListenerBase {
         mAnomalyReporterAdapter = anomalyReporterAdapter;
         mLock = new Object();
         mFeatureFlags = featureFlags;
+        mSensitivePhoneNumbers = SensitivePhoneNumbers.getInstance();
     }
 
     @Override
@@ -436,16 +440,20 @@ public final class CallLogManager extends CallsManagerListenerBase {
         boolean okToLogEmergencyNumber = false;
         CarrierConfigManager configManager = (CarrierConfigManager) mContext.getSystemService(
                 Context.CARRIER_CONFIG_SERVICE);
-        PersistableBundle configBundle = configManager.getConfigForSubId(
-                mPhoneAccountRegistrar.getSubscriptionIdForPhoneAccount(accountHandle));
+        int subId = mPhoneAccountRegistrar.getSubscriptionIdForPhoneAccount(accountHandle);
+        PersistableBundle configBundle = configManager.getConfigForSubId(subId);
         if (configBundle != null) {
             okToLogEmergencyNumber = configBundle.getBoolean(
                     CarrierConfigManager.KEY_ALLOW_EMERGENCY_NUMBERS_IN_CALL_LOG_BOOL);
         }
 
+        // Don't log sensitive numbers.
+        boolean isSensitiveNumber = mSensitivePhoneNumbers.isSensitiveNumber(mContext, number,
+                subId);
+
         // Don't log emergency numbers if the device doesn't allow it.
         return (!isEmergency || okToLogEmergencyNumber)
-                && !isUnloggableNumber(number, configBundle);
+                && !isUnloggableNumber(number, configBundle) && !isSensitiveNumber;
     }
 
     private boolean isUnloggableNumber(String callNumber, PersistableBundle carrierConfig) {
